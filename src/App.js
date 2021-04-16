776697f94636182f5f0eb91cc66120bc65352268
kushendra1 <kushendra.ramrup59@myhunter.cuny.edu>
2021-04-15 20:58:28 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { pokemon } from "./components/pokemon"
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [pokemon, setPokemon] = useState("");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const handleSearch = async () => {
+      if (pokemon === "") return;
+      let linkToAPI = "https://pokeapi.co/api/v2/pokemon/" + pokemon;
+
+      try {
+        let response = await axios.get(linkToAPI);
+        setData(response.data);
+      } catch (error) {
+        if (error.response) {
+          console.log(error.response.data);
+          console.log(error.response.status);
+        }
+      }
+    };
+    handleSearch();
+  }, [pokemon]);
+
+  const generateData = () => {
+    return data.map((id) => {
+      return <data key={id.name} {...city} />;
+    });
+  };
+
+  const handleChange = (e) => {
+    setPokemon(e.target.value)
+  }
+
+  return {
+    <div id="content-wrapper">
+      <div id="header">
+        <div id="header-text">
+          <h1>Pokemon Look-Up</h1>
+        </div>
+      </div>
+        <div id="search-box">
+          <label>
+            Enter Pokemon:
+          </label>
+          <input className="search-box-field" placeholder="Enter a zipcode!" type="text" onChange={handleChange} />
+        </div>
+        {generateData()}
+      </div>
+  };
+
 }
-
 export default App;
