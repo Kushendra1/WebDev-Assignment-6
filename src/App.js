8f3c667f607680b1f7f756cd8342728674058377
ftanmoy <43865401+ftanmoy@users.noreply.github.com>
2021-04-16 07:07:48 -0400

    boiler and api connect

--- src/App.js
+++ src/App.js
@@ -1,56 +1,89 @@
-import { useState, useEffect } from "react";
+import { Component } from "react";
 import axios from "axios";
-import { pokemon } from "./components/pokemon"
 import "./App.css";
 
-const App = () => {
-  const [pokemon, setPokemon] = useState("");
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const handleSearch = async () => {
-      if (pokemon === "") return;
-      let linkToAPI = "https://pokeapi.co/api/v2/pokemon/" + pokemon;
-
-      try {
-        let response = await axios.get(linkToAPI);
-        setData(response.data);
-      } catch (error) {
-        if (error.response) {
-          console.log(error.response.data);
-          console.log(error.response.status);
-        }
-      }
-    };
-    handleSearch();
-  }, [pokemon]);
-
-  const generateData = () => {
-    return data.map((id) => {
-      return <data key={id.name} {...city} />;
-    });
-  };
-
-  const handleChange = (e) => {
-    setPokemon(e.target.value)
+class SearchAPI extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      apiData: [],
+      pokemon: "",
+      found: false
+    }
   }
 
-  return {
-    <div id="content-wrapper">
-      <div id="header">
-        <div id="header-text">
-          <h1>Pokemon Look-Up</h1>
-        </div>
-      </div>
-        <div id="search-box">
-          <label>
-            Enter Pokemon:
-          </label>
-          <input className="search-box-field" placeholder="Enter a zipcode!" type="text" onChange={handleChange} />
-        </div>
-        {generateData()}
-      </div>
-  };
+  handleChange = (event) => {
+    this.setState({pokemon: event.target.value});
+  }
 
+  handleSearchClick = async () => {
+    let pokemonName = this.state.pokemon;
+    let linkToAPI = 'https://pokeapi.co/api/v2/pokemon/' + pokemonName;
+
+    try{
+      let response = await axios.get(linkToAPI);
+      this.setState({apiData: response.data, found: true});
+    }
+    catch (error){
+      if(error.response){
+        console.log(error.response.data);
+        console.log(error.response.status);
+        this.setState({found: false});
+      }
+    }
+  }
+
+  generateData = () =>{
+    let pokemonData = this.state.apiData;
+    let foundResults = this.state.found;
+    let typesData = [pokemonData.types];
+    let table = [];
+
+    if(!foundResults){
+      <p>No results.</p>
+    }
+    else{
+      let name = pokemonData.name;
+      let height = pokemonData.height;
+      let weight = pokemonData.weight;
+      let experience = pokemonData.base_experience;
+      let species = pokemonData.species.name;
+      let type = typesData;
+      let abilities = pokemonData.abilities.name;
+
+      table.push(
+        <tr key = {pokemonData.id}>
+          <td>Height: {height}</td>
+          <td>Weight: {weight}</td>
+          <td>Experience: {experience}</td>
+          <td>Species: {species}</td>
+          <td>Type: {type}</td>
+          <td>Abilities: {abilities}</td>
+        </tr>
+      );
+      return table;
+    }
+  }
+
+  render(){
+    return (
+      <div className = 'container'>
+        <div className = 'search'>
+          <h3>Pokemon Search</h3>
+          <input type="text" value={this.state.pokemon} onChange = {this.handleChange} placeholder = 'Enter Pokemon Name!'></input>
+          <button className="search-button" onClick={this.handleSearchClick}>I Choose You!</button>
+        </div>
+        <br/>
+        <h4>{this.state.pokemon.name}</h4>
+        <table id = "data">
+          <tbody>
+          {this.generateData()}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 }
-export default App;
+
+export default SearchAPI;
+
--- src/components/pokemon.jsx
+++ src/components/pokemon.jsx
@@ -1,1 +1,17 @@
 import React from 'react'
+
+export const pokemonAttributes = ({ name, height, weight, abilities, types, species, base_experience }) => {
+    return (
+      <table>
+        <tr key = {pokemonData.id}>
+          <td><strong>{name}</strong></td>
+          <td>Height: {height}</td>
+          <td>Weight: {weight}</td>
+          <td>Experience: {experience}</td>
+          <td>Species: {species}</td>
+          <td>Type: {type}</td>
+          <td>Abilities: {abilities}</td>
+        </tr>
+      </table>
+    )
+}
